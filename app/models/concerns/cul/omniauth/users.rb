1aba210ff26bac7fb55dc98f6d9ad4e5f6ac9890
Benjamin Armintor <armintor@gmail.com>
2015-08-19 17:08:07 -0400

    DRY users

--- app/models/concerns/cul/omniauth/users.rb
+++ app/models/concerns/cul/omniauth/users.rb
@@ -29,44 +29,33 @@ module Cul::Omniauth::Users
   end
 
   module ClassMethods
-    def find_for_cas(token, resource=nil)
-      user = where(:uid => token.uid).first
+    def find_for_provider(token, provider)
+      return nil unless token.uid
+      props = {:uid => token.uid.downcase, provider: provider.downcase}
+      user = where(props).first
       # create new user if necessary
       unless user
-        user = create(whitelist(:uid => token.uid))
+        user = create!(whitelist(props))
         # can we add groups or roles here?
       end
       user
+    end
+    def find_for_cas(token, resource=nil)
+      find_for_provider(token, 'cas')
     end
 
     def find_for_saml(token, resource=nil)
-      user = where(:uid => token.uid).first
-      # create new user if necessary
-      unless user
-        user = create(whitelist(:uid => token.uid))
-        # can we add groups or roles here?
-      end
-
-      user
+      find_for_provider(token, 'saml')
     end
 
     def find_for_wind(token, resource=nil)
-      user = where(:uid => token.uid).first
-      # create new user if necessary
-      unless user
-        user = create(whitelist(:uid => token.uid))
-        # can we add groups or roles here?
-      end
-
-      user
+      find_for_provider(token, 'wind')
     end
 
     def from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      where(provider: auth.provider.downcase, uid: auth.uid.downcase).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
-        user.name = auth.info.name   # assuming the user model has a name
-        user.image = auth.info.image # assuming the user model has an image
       end
     end
 
---
+++ spec/lib/cul/omniauth/users_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe Cul::Omniauth::Users do
+
+  it do
+    is_expected.to be_a Module
+  end
+  context "is included" do
+    let(:token){ OpenStruct.new(uid: uid, provider: provider) }
+    let(:uid) { 'foo' }
+    let(:provider) { 'lol' }
+    subject { User }
+    context "token has no uid" do
+      before do
+        token.uid = nil
+      end
+      it do
+        expect(subject.find_for_provider(token, provider)).not_to be
+      end
+    end
+    context "existing user" do
+      let(:users) { [double(User)]}
+      it do
+        is_expected.to receive(:where).with(uid: uid, provider: provider).and_return(users)
+        is_expected.not_to receive(:"create!")
+        subject.find_for_provider(token, provider)
+      end
+    end
+    context "new user" do
+      let(:users) { []}
+      it do
+        is_expected.to receive(:where).with(uid: uid, provider: provider).and_return(users)
+        is_expected.to receive(:"create!").with(uid: uid, provider: provider).and_return(double(User))
+        subject.find_for_provider(token, provider)
+      end
+    end
+    ["cas", "saml", "wind"].each do |method|
+      context "find with #{method} provider" do
+        it do
+          is_expected.to receive(:find_for_provider).with(token,method)
+          subject.send :"find_for_#{method}", token
+        end
+      end
+    end
+  end
+end
