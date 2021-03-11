4af5b8f70878fa7494d9eb87157bf79bc48d00da
Stuart Marquis <stuartmarquis@gmail.com>
2021-03-11 10:15:22 -0500

    comment out redirect_url from failure_app.rb

--- lib/cul/omniauth/failure_app.rb
+++ lib/cul/omniauth/failure_app.rb
@@ -12,7 +12,9 @@ class Cul::Omniauth::FailureApp < Devise::FailureApp
   	r.provider = provider || self.provider
   	r
   end
-  def redirect_url
-    user_omniauth_authorize_path(self.class.provider)
-  end
+
+  #def redirect_url
+  #  user_omniauth_authorize_path(self.class.provider)
+  #end
+
 end
